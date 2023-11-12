9cbed86f4ab11fd32427ec9f59af84a647abddfc
buatorng14 <150588311+buatorng14@users.noreply.github.com>
2023-11-12 20:37:37 +0700

    Update_app.py

--- app.py
+++ app.py
@@ -1,21 +1,33 @@
+
 import streamlit as st
 import sqlite3
+import ast
 
-# ติดต่อฐานข้อมูล SQL
-conn = sqlite3.connect('tong.db')
-cursor = conn.cursor()  # สร้าง cursor
+def delete_order(order_id):
+    conn = sqlite3.connect('tong.db')
+    cursor = conn.cursor()
 
-# ปฏิบัติการ SQL 
-cursor.execute("SELECT IDproduct, NameProduct, PricePerUnit, Image FROM ProduceInfo")
-data = cursor.fetchall()
+    # Perform the deletion logic here
+    cursor.execute('DELETE FROM Orders WHERE OrderCode=?', (order_id,))
+    conn.commit()
 
-# Create a dictionary to store product information in the shopping cart
-st.write(data)
-
-
-
-
-
+with sqlite3.connect('tong.db') as conn:
+    cursor = conn.cursor()
+    cursor.execute("SELECT OrderCode, Product, TotalPrice, CustomerNote FROM Orders")
+    data = cursor.fetchall()
+    st.title('รายการคำสั่งซื้อทั้งหมด')
+    for product in data:
+        OrderCode, Product, TotalPrice, CustomerNote = product
+        product_dict = ast.literal_eval(Product)
+        with st.form(key=f'form_{OrderCode}'):
+            st.write(OrderCode)
+            st.write("**สินค้า:**")
+            for item_name, item_quantity in product_dict.items():
+                st.write(f"- {item_name} จำนวน {item_quantity}")
+            st.write(f'**ราคารวม:** :red[{TotalPrice} ฿]')
+            st.write(f'**โน้ตถึงร้านค้า:** {CustomerNote}')
+            if st.form_submit_button(label='Finish', use_container_width=True):
+                delete_order(OrderCode)
         
 
 
